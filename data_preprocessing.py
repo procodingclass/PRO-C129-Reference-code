3eda6e5631cb74554c8bf83141057f1b9c001eee
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-10 00:34:59 +0530

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,18 @@
+# import csv
+import csv
+  
+# open input CSV file as source
+
+with open("final.csv", "r") as source:
+    reader = csv.reader(source)
+
+# open output CSV file as result    
+    with open("final_1.csv", "w") as result:
+        writer = csv.writer(result)
+        for row in reader:
+            if row!='':
+                writer.writerow((row[0], row[1], row[2], row[3], row[4],row[5], row[6], row[9]))
+            else:
+                pass
+
+
